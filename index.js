bf69926a09f69d1d1e0159640895ad793af23203
GitMonies <jarod.smelosky@yahoo.com>
2023-09-08 15:26:40 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+// function noParameter() {
+//     greeting = "How are you?"
+//     alert(greeting + " " + "I'm well!")
+// }
+
+// noParameter()
+
+
+// function dayGreeting(name, time) {
+// let message = alert("Hello, my name is "+ name + ", it's currently " + time + ".")
+// }
+
+// let time = "5:00pm"
+
+// dayGreeting('John', time)
+
+// alert(message)
+
+
+
+//  function sumNumber (num1, num2) {
+//     return num1 + num2
+//  }
+
+//  let sum = sumNumber(49, 273)
+//  alert(sum)
